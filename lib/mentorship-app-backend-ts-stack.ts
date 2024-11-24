d2f7b1b45ff3bb1738adb74a0c2fd60e69c09820
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-24 15:37:10 +0400

    remove unnecssary imports

--- lib/mentorship-app-backend-ts-stack.ts
+++ lib/mentorship-app-backend-ts-stack.ts
@@ -1,13 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as cognito from 'aws-cdk-lib/aws-cognito';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-// @ts-ignore
-import * as origins from '@aws-cdk/aws-cloudfront-origins';
 import { Config } from '../config/config';
 import { InitializeBucket } from '../components/s3-bucket/initializer';
 import { InitializeProfileTable } from '../components/dynamo-db/initializer';
@@ -31,15 +23,12 @@ export class MentorshipAppBackendTsStack extends cdk.Stack {
 
     console.log(`Initializing stack for environment: ${config.environment}`);
 
-    // S3 Bucket
     const s3Bucket = InitializeBucket(this, config.bucketName);
     console.log(`Bucket Name: ${s3Bucket.bucketName}`);
 
-    // DynamoDB Table
     const removalPolicy = config.environment === 'staging' ? cdk.RemovalPolicy.DESTROY : cdk.RemovalPolicy.RETAIN;
     const profileTable = InitializeProfileTable(this, config.userProfileDDBTableName, removalPolicy);
 
-    // Lambda Functions
     const uploadLambda = InitializeLambda(this, s3Bucket, profileTable, UploadLambdaName,{}, config);
     const lambdas = {
       upload: uploadLambda,
@@ -53,14 +42,11 @@ export class MentorshipAppBackendTsStack extends cdk.Stack {
       resend: InitializeLambda(this, s3Bucket, profileTable, ResendLambdaName,{}, config),
     };
 
-    // Cognito User Pool and Authorizer
     const userPool = InitializeUserPool(this, config.userPoolName, config.cognitoPoolArn);
     const cognitoAuthorizer = InitializeCognitoAuthorizer(this, config.cognitoAuthorizer, userPool);
 
-    // API Gateway
     const apiInstance = InitializeAPI(this, lambdas, cognitoAuthorizer, config.environment);
 
-    // CloudFront Distribution
     CreateCloudFrontDistribution(this, apiInstance, config.environment);
 
     console.log('Stack initialization complete.');
