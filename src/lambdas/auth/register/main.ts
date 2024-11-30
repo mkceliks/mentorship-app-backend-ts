cea13e996fe51b96c26bf95d680d4f8de7b1c178
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-30 19:26:25 +0400

    slack name updated with channel id

--- src/lambdas/auth/register/main.ts
+++ src/lambdas/auth/register/main.ts
@@ -148,4 +148,4 @@ function extractUserPoolID(cognitoPoolArn: string): string {
 /**
  * The main handler function wrapped with handlerWrapper for Slack notifications and logging.
  */
-export const handler = handlerWrapper(RegisterHandler, 'auth-cognito', 'RegisterHandler');
+export const handler = handlerWrapper(RegisterHandler, 'C07UWDMBWQP', 'RegisterHandler');
